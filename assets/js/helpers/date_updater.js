59f87e4f0c8a16153392f108306f93e3d5d94b00
edgarFlores <ragde.gfc@gmail.com>
2024-10-17 20:50:45 -0700

    update

---
+++ assets/js/helpers/date_updater.js
@@ -0,0 +1,9 @@
+function updateDateYear() {
+	const currentYear = new Date().getFullYear();
+
+	const copyrightElement = document.querySelector('.footer__copy');
+
+	copyrightElement.textContent = `© ${currentYear}, All rights reserved.`;
+}
+
+export default updateDateYear;
