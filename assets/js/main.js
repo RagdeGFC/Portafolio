870b5692e75d25d9128a01db1e08e14f2590850b
RagdeGFC <ragde.gfc@gmail.com>
2025-04-12 08:59:52 -0700

    2ndCommit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-import parallax from './components/parallax.js';
-import activeMenu from './components/selected_menu.js';
-import updateDateYear from './helpers/date_updater.js';
-import changeLang from './helpers/language.js';
-import loader from './helpers/loader.js';
-import resetToHome from './helpers/reload_page.js';
+import parallax from './components/parallax.js'; //
+import activeMenu from './components/selected_menu.js'; //
+import updateDateYear from './helpers/date_updater.js'; //
+import changeLang from './helpers/language.js'; //
+import loader from './helpers/loader.js'; //
+import resetToHome from './helpers/reload_page.js'; //
 import sendEmail from './helpers/send_form.js';
 
 loader();
